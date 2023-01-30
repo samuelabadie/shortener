2466a6187d3726f5fe520c003e9eeb619854c1e7
samuelabadie <91666297+samuelabadie@users.noreply.github.com>
2023-01-30 16:06:57 +0100

    feat: centering input

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,15 +1,15 @@
 class Link < ApplicationRecord
-
+  
   validates_presence_of :lookup_code, :original_url
   validates_uniqueness_of :lookup_code
   validate :original_url_format
-
+  
   def short_url
     "http://#{ENV.fetch('HOST')}/#{lookup_code}"
   end
   
   private
-
+  
   def original_url_format
     uri = URI.parse(original_url || '') # parse => #<URI::HTTP:0x202281be URL:http://www.ruby-lang.org/> 
     if uri.host.nil? #uri.host = "www.google.com"
